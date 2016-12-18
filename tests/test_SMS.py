b5037c25d7330b61b0c6b93ef46157c1ef793762
tzhenghao <tanzhao@umich.edu>
2016-12-17 19:37:32 -0600

    fixed SID and Token

--- tests/test_SMS.py
+++ tests/test_SMS.py
@@ -6,8 +6,8 @@ sys.path.append("..")
 
 import SMS
 
-accountSID = 'AC3ab635e624b006998a8f09ae9b2e4695'
-authToken = 'd888435c644791874e677574439357aa'
+accountSID = 'bla1'
+authToken = 'bla2'
 
 class TestSMS(object):
 
